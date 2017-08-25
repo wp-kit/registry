0cd268a8b6b678d2204398d81ce30cd24f01046d
terence1990 <terence@creativelittledots.co.uk>
2017-08-25 16:30:11 +0100

    upgrade for wp-kit

--- src/Registry/PostType.php
+++ src/Registry/PostType.php
@@ -6,4 +6,34 @@ class PostType extends Registration {
     
     var $taxonomies = array();
     
+    protected function getDefaultOptions() {
+	    
+	    return array(
+			'rewrite' 		=> array( 'slug' => sanitize_title( $this->slug ) ),
+			'has_archive'   => sanitize_title( $this->plural ),
+	    );
+	    
+    }
+    
+    protected function getDefaultLabels() {
+	    
+	    return array(
+			'name'               => _x( $this->plural, $this->slug, 'wpkit' ),
+			'singular_name'      => _x( $this->name, $this->slug, 'wpkit' ),
+			'menu_name'          => _x( $this->plural, 'admin menu', 'wpkit' ),
+			'name_admin_bar'     => _x( $this->name, 'add new on admin bar', 'wpkit' ),
+			'add_new'            => _x( 'Add New', $this->slug, 'wpkit' ),
+			'add_new_item'       => __( 'Add New '. $this->name, 'wpkit' ),
+			'new_item'           => __( 'New ' . $this->name, 'wpkit' ),
+			'edit_item'          => __( 'Edit ' . $this->name, 'wpkit' ),
+			'view_item'          => __( 'View ' . $this->name, 'wpkit' ),
+			'all_items'          => __( 'All '. $this->plural, 'wpkit' ),
+			'search_items'       => __( 'Search ' . $this->plural, 'wpkit' ),
+			'parent_item_colon'  => __( 'Parent ' . $this->plural . ':', 'wpkit' ),
+			'not_found'          => __( 'No ' . strtolower( $this->plural ) . ' found.', 'wpkit' ),
+			'not_found_in_trash' => __( 'No ' . strtolower( $this->plural ) . ' found in Trash.', 'wpkit' )
+		);
+	    
+    }
+    
 }
--- src/Registry/Taxonomy.php
+++ src/Registry/Taxonomy.php
@@ -2,6 +2,8 @@
 
 namespace WPKit\Registry;
 
+use Doctrine\Common\Inflector\Inflector;
+
 class Taxonomy extends Registration {
     
     public $post_types = array();
@@ -11,6 +13,32 @@ class Taxonomy extends Registration {
 	    parent::__construct( $args );
 	    
 	    $this->options['labels'] = $this->labels;
+	    
+    }
+    
+    protected function getDefaultOptions() {
+	    
+	    return array(
+			'rewrite'           => array( 'slug' => sanitize_title( $this->slug ) ),
+	    );
+	    
+    }
+    
+    protected function getDefaultLabels() {
+	    
+	    return array(
+			'name'              => _x( $this->plural, 'wpkit' ),
+			'singular_name'     => _x( $this->name, 'wpkit' ),
+			'search_items'      => __( 'Search '.$this->plural ),
+			'all_items'         => __( 'All ' . $this->plural ),
+			'parent_item'       => __( 'Parent ' . $this->name ),
+			'parent_item_colon' => __( 'Parent ' . $this->name.':' ),
+			'edit_item'         => __( 'Edit ' . $this->name ),
+			'update_item'       => __( 'Update ' . $this->name ),
+			'add_new_item'      => __( 'Add New ' . $this->name ),
+			'new_item_name'     => __( 'New ' . $this->name . ' Name' ),
+			'menu_name'         => __( $this->plural ),
+		);
 	    
     }
     

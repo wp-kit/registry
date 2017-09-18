5ba0e672c7aa04b25691d0dfd895cf705d70a9b9
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 10:43:30 +0100

    Update Taxonomy.php

--- src/Registry/Taxonomy.php
+++ src/Registry/Taxonomy.php
@@ -49,15 +49,15 @@ class Taxonomy extends Registration {
 	    
 	    return array(
 			'name'              => _x( $this->plural, 'wpkit' ),
-			'singular_name'     => _x( $this->name, 'wpkit' ),
+			'singular_name'     => _x( $this->singular, 'wpkit' ),
 			'search_items'      => __( 'Search '.$this->plural ),
 			'all_items'         => __( 'All ' . $this->plural ),
-			'parent_item'       => __( 'Parent ' . $this->name ),
-			'parent_item_colon' => __( 'Parent ' . $this->name.':' ),
-			'edit_item'         => __( 'Edit ' . $this->name ),
-			'update_item'       => __( 'Update ' . $this->name ),
-			'add_new_item'      => __( 'Add New ' . $this->name ),
-			'new_item_name'     => __( 'New ' . $this->name . ' Name' ),
+			'parent_item'       => __( 'Parent ' . $this->singular ),
+			'parent_item_colon' => __( 'Parent ' . $this->singular.':' ),
+			'edit_item'         => __( 'Edit ' . $this->singular ),
+			'update_item'       => __( 'Update ' . $this->singular ),
+			'add_new_item'      => __( 'Add New ' . $this->singular ),
+			'new_item_name'     => __( 'New ' . $this->singular . ' Name' ),
 			'menu_name'         => __( $this->plural ),
 		);
 	    

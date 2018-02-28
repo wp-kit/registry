13681239adca6876c3af60eb48b0325a177080ae
Terence O'Donoghue <terence@creativelittledots.co.uk>
2018-02-28 18:16:03 +0000

    Update Test.php

--- postTypes/Test.php
+++ postTypes/Test.php
@@ -5,17 +5,55 @@ namespace Theme\Taxonomies;
 use WPKit\Registry\PostType;
 
 class Test extends PostType {
-  var $name = 'test';
-  var $plural = 'tests';
-  var $singular = 'test';
-  var $options = [
-    'supports' => [ 'title', 'editor', 'thumbnail', 'page-attributes', 'revisions' ]
-  ];
-  var $labels = [
-    'menu_name' => 'The Tests',
-  ];
-  var $taxonomies = [
-    'cat',
-    'post_tag'
-  ];
+  
+	/**
+	* The name of the registration
+	*
+	* @var string
+	*/
+	var $name = 'test';
+	
+	/**
+	* The plural name of the registration
+	*
+	* @var string
+	*/
+	var $plural = 'tests';
+	
+	/**
+	* The singular name of the registration
+	*
+	* @var string
+	*/
+	var $singular = 'test';
+	
+	/**
+	* The options of the registration
+	*
+	* @var array
+	* https://codex.wordpress.org/Function_Reference/register_post_type#Arguments
+	*/
+	var $options = [
+		'supports' => [ 'title', 'editor', 'thumbnail', 'page-attributes', 'revisions' ]
+	];
+	
+	/**
+	* The labels of the registration
+	*
+	* @var array
+	* https://codex.wordpress.org/Function_Reference/register_post_type#labels
+	*/
+	var $labels = [
+		'menu_name' => 'The Tests',
+	];
+	
+	/**
+	* The slug of taxonomies in this registration
+	*
+	* @var array
+	*/
+	var $taxonomies = [
+		'cat',
+		'post_tag'
+	];
 }

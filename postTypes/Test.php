8b304a9f087f94a12469e38c3d2ee898e502739d
Terence O'Donoghue <terence@creativelittledots.co.uk>
2018-02-28 17:22:57 +0000

    Update Test.php

--- postTypes/Test.php
+++ postTypes/Test.php
@@ -5,5 +5,17 @@ namespace Theme\Taxonomies;
 use WPKit\Registry\PostType;
 
 class Test extends PostType {
-
+  var $name = 'test';
+  var $plural = 'tests';
+  var $singular = 'test';
+  var $options = [
+    'supports' => [ 'title', 'editor', 'thumbnail', 'page-attributes', 'revisions' ]
+  ];
+  var $labels = [
+    'menu_name' => 'The Tests',
+  ];
+  var $taxonomies = [
+    'cat',
+    'post_tag'
+  ];
 }
